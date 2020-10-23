52f03df80c1607d2209e2cdf35dbea8f79435809
NickCommit <shushugin@gmail.com>
2020-10-23 21:31:16 +0300

    result

--- src/components/stats.vue
+++ src/components/stats.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="stats-component">
     <h3>Аналитика по визитам</h3>
-
+    <br/>
+    <div class="container">
+      <chart :chartdata="chartData" :options="chartOptions" />
+    </div>
   </div>
 </template>
 
 <script>
+import chart from "../components/chart.vue";
+
 export default {
-  mounted: function () {
-    if (!localStorage.getItem('key')) {
-      this.$router.push('/auth')
-    }
+  components: {
+    chart
   },
-
-}
+  mounted: function() {
+    if (!localStorage.getItem("key")) {
+      this.$router.push("/auth");
+    }
+  }
+};
 </script>
+<style lang="scss">
 
-<style>
-
-</style>
+</style>>
